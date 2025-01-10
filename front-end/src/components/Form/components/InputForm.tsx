ce26dfef807808de8f32a0b430824d7ad8932beb
Jeremy <jeremy.ramirez.p@gmail.com>
2025-01-10 11:07:19 -0500

    fixed: component and packgage

--- front-end/src/components/Form/components/InputForm.tsx
+++ front-end/src/components/Form/components/InputForm.tsx
@@ -17,13 +17,13 @@ export const InputForm = ({ name, control, label, type, error }: Props) => {
             name={name}
             control={control}
             render={({ field }) => {
+
                 return <>
                     { name !== "status" && (
-                        <input
+                        <input 
                             type={type} 
                             id={name} 
                             {...field } 
-                            value={field.value instanceof Date ? field.value.toISOString().split('T')[0] : field.value ?? ''}
                             className="flex h-9 w-full rounded-md border border-input bg-transparent px-3 py-1 text-base shadow-sm transition-colors file:border-0 file:bg-transparent file:text-sm file:font-medium file:text-foreground placeholder:text-muted-foreground focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 md:text-sm" 
                         />
                     )}
